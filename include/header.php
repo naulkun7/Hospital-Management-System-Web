cfa30e6350d9e5e5a19c36622d7971f898bb844c
naulkun7 <luannguyenn.2k2@gmail.com>
2023-04-27 02:21:01 +0700

    update2

--- include/header.php
+++ include/header.php
@@ -4,7 +4,8 @@
 
 <style>
 <?php include("asset/css/style.css");
-include("asset/css/base.css") ?>
+include("asset/css/base.css");
+include("asset/css/header.css") ?>
 </style>
 
 <!DOCTYPE html>
@@ -15,18 +16,21 @@ include("asset/css/base.css") ?>
   <title>Hospital Database Management</title>
   <link rel="stylesheet" type="text/css"
     href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <script src="https://code.jquery.com/jquery-3.6.0.slim.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-info bg-info">
-    <h5 class="text-white">Hospital Management System</h5>
-    <div class="mr-auto"></div>
-    <ul class="navbar-nav">
-      <?php
+    <div class="container">
+      <i class="fa-solid fa-house icon text-white">
+        HOSPITAL MANAGEMENT SYSTEM
+      </i>
+      <div class="mr-auto"></div>
+      <ul class="navbar-nav">
+        <?php
         if (isset($_SESSION["admin"])) {
           $user = $_SESSION["admin"];
           echo '
@@ -54,7 +58,8 @@ include("asset/css/base.css") ?>
           ';
         }
       ?>
-    </ul>
+      </ul>
+    </div>
   </nav>
 </body>
 
--- index.php
+++ index.php
@@ -18,21 +18,23 @@ include("asset/css/base.css") ?>
 
 <body>
   <div class="section1">
-    <div class="row d-flex">
-      <div class="col-6">
-        <div class="box1">
-          <h4 class="title">
-            WELCOME TO OUR PROJECT
-          </h4>
-          <h1 class="sub-title">
-            Hospital Management System
-          </h1>
-          <a class="button button-yellow" href="#">GO</a>
+    <div class="container">
+      <div class="row d-flex">
+        <div class="col-6">
+          <div class="box1">
+            <h4 class="title">
+              WELCOME TO OUR PROJECT
+            </h4>
+            <h1 class="sub-title">
+              Hospital Management System
+            </h1>
+            <a class="button button-yellow" href="#">GO</a>
+          </div>
         </div>
-      </div>
-      <div class="col-6">
-        <div class="box2">
-          <img src="img/product1.jpg" class="image" alt="picture-sec1">
+        <div class="col-6">
+          <div class="box2">
+            <img src="img/product1.jpg" class="image" alt="picture-sec1">
+          </div>
         </div>
       </div>
     </div>
@@ -61,7 +63,7 @@ include("asset/css/base.css") ?>
               <p class="desc">
                 Create an account which we can support and take good care of you.
               </p>
-              <a href="./nav/patientaccount.php" class="button button-yellow">REGISTER</a>
+              <a href="patientaccount.php" class="button button-yellow">REGISTER</a>
             </div>
           </div>
         </div>
@@ -84,17 +86,51 @@ include("asset/css/base.css") ?>
   </div>
 
   <div class="section3">
-    <div class="inner-content">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.131719788131!2d106.79904467588085!3d10.877584789277451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d8a415a9d221%3A0x550c2b41569376f9!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBRdeG7kWMgVOG6vyAtIMSQ4bqhaSBo4buNYyBRdeG7kWMgZ2lhIFRQLkhDTQ!5e0!3m2!1svi!2s!4v1682512870711!5m2!1svi!2s"
-        width="600" height="450" style="border:0; width:100%" allowfullscreen="" loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="container">
+      <div class="inner-content">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.131719788131!2d106.79904467588085!3d10.877584789277451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d8a415a9d221%3A0x550c2b41569376f9!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBRdeG7kWMgVOG6vyAtIMSQ4bqhaSBo4buNYyBRdeG7kWMgZ2lhIFRQLkhDTQ!5e0!3m2!1svi!2s!4v1682512870711!5m2!1svi!2s"
+          width="600" height="450" style="border:0; width:100%" allowfullscreen="" loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
     </div>
   </div>
 
   <footer>
-    <div class="inner">
-      .inner
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <div id="copyright">
+            <h3>COPYRIGHT BY COOK GROUP</h3>
+          </div>
+        </div>
+        <div class="col-6">
+          <ul id="social">
+            <li class="item">
+              <a href="#">
+                <i class="fa-brands fa-github">
+                  GITHUB
+                </i>
+              </a>
+            </li>
+            <li class="item">
+              <a href="#">
+                <i class="fa-brands fa-facebook">
+                  FACEBOOK
+                </i>
+              </a>
+            </li>
+            <li class="item">
+              <a href="tel:115">
+                <i class="fa-solid fa-phone">
+                  Tel: 115
+                </i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
     </div>
   </footer>
 </body>
