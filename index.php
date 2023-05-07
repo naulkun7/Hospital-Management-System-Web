900c288cf17e5b2f4e6c321b2319ccf3c5bff73a
naulkun7 <luannguyenn.2k2@gmail.com>
2023-05-07 12:57:20 +0700

    update ui main page

--- index.php
+++ index.php
@@ -43,41 +43,16 @@ include("asset/css/base.css") ?>
   <div class="section9">
     <div class="container">
       <div class="row row2">
-        <div class="col-lg-4 col-md-6">
-          <div class="tag" id="moreinfo">
-            <img src="./img/moreinfo.jpg" alt="blog-item-01" />
-            <div class="tag-content">
-              <a href="#" class="title">More information</a>
-              <p class="desc">
-                Click on the button to read report and all features of our project
-              </p>
-              <a href="https://drive.google.com/drive/folders/1hQIMl28yTX1B-RTUwq1cX2lPGwLK1wGC"
-                class="button button-yellow" target="_blank">READ MORE</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
+        <div class="col-lg-12 col-md-12">
           <div class="tag" id="register">
             <img src="./img/registerpatient.jpg" alt="blog-item-01" alt="blog-item-02" />
             <div class="tag-content">
-              <a href="#" class="title">Register Patient</a>
-              <p class="desc">
+              <a href="#" class="title" style="font-weight: 700; font-size: 40px">REGISTER PATIENT</a>
+              <p class="desc" style="font-size: 30px">
                 Create an account which we can support and take good care of you.
               </p>
-              <a href="patientaccount.php" class="button button-yellow">REGISTER</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-          <div class="tag">
-            <img src="./img/contactus.jpg" alt="blog-item-01" alt="blog-item-02" />
-
-            <div class="tag-content">
-              <a href="#" class="title" target="_blank">Contact Us</a>
-              <p class="desc">
-                All information <br> about us!
-              </p>
-              <a href="#" class="button button-yellow">DANGER</a>
+              <a href="patientaccount.php" class="button button-blue"
+                style="font-size:20px"><strong>REGISTER</strong></a>
             </div>
           </div>
         </div>
@@ -86,13 +61,44 @@ include("asset/css/base.css") ?>
   </div>
   </div>
 
+
+
+
   <div class="section3">
     <div class="container">
-      <div class="inner-content">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.131719788131!2d106.79904467588085!3d10.877584789277451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d8a415a9d221%3A0x550c2b41569376f9!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBRdeG7kWMgVOG6vyAtIMSQ4bqhaSBo4buNYyBRdeG7kWMgZ2lhIFRQLkhDTQ!5e0!3m2!1svi!2s!4v1682512870711!5m2!1svi!2s"
-          width="600" height="450" style="border:0; width:100%" allowfullscreen="" loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <div class="row">
+        <div class="col-6">
+          <div class="map">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.131719788131!2d106.79904467588085!3d10.877584789277451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d8a415a9d221%3A0x550c2b41569376f9!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBRdeG7kWMgVOG6vyAtIMSQ4bqhaSBo4buNYyBRdeG7kWMgZ2lhIFRQLkhDTQ!5e0!3m2!1svi!2s!4v1682512870711!5m2!1svi!2s"
+              width="600" height="450" style="border:0; width:100%" allowfullscreen="" loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="contact">
+            <h2 class="title">
+              CONTACT US
+            </h2>
+            <ul class="listMember">
+              <li class="member">Nguyễn Hoàng Thiên Phúc - ITITIU20279</li>
+              <li class="member">Nguyễn Hoàng Luân - ITITIU20012</li>
+              <li class="member">Trần Quang Trí - ITITIU20325</li>
+              <li class="member">Nguyễn Lê Thành Tài - ITITIU20298</li>
+              <li class="member">Nghĩa</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="more_info">
+    <div class="container">
+      <div class="content">
+        <a href="">
+          MORE INFORMATION
+        </a>
       </div>
     </div>
   </div>
